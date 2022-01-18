4fd970a3d9a44d88f12bcaa6895a9af1384b163f
Harriss1989 <harriss_1@hotmail.co.uk>
2022-01-18 11:05:46 +0000

    add buttons link js file

---
+++ assets/js/script.js
@@ -0,0 +1,14 @@
+const selectionButtons = document.querySelectorAll('[data-selection]')
+
+
+selectionButtons.forEach(selectionButton => {
+    selectionButton.addEventListener('click', e => {
+        const selectionName = selectionButton.dataset.selection
+        makeSelection(selectionName)
+    })
+})
+
+
+function makeSelection(selection) {
+    console.log(selection)
+}
