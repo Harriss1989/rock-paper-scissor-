46e5e050543de92ba43c778c4365d26e36aaaecf
Harriss1989 <harriss_1@hotmail.co.uk>
2022-01-18 14:18:36 +0000

    add functionailty to game

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,14 +1,52 @@
 const selectionButtons = document.querySelectorAll('[data-selection]')
-
+const finalColumn = document.querySelector('[data-final-column]')
+const SElECTIONS = [
+ {
+    name: 'rock',
+    beats: 'scissors',
+ },
+ {
+    name: 'paper',
+    beats: 'rock',
+ },
+ {
+    name: 'scissors',
+    beats: 'paper',
+ }
+]
 
 selectionButtons.forEach(selectionButton => {
     selectionButton.addEventListener('click', e => {
         const selectionName = selectionButton.dataset.selection
-        makeSelection(selectionName)
+        const selection = SElECTIONS.find(selection => selection.name === selectionName)
+        makeSelection(selection)
     })
 })
 
 
 function makeSelection(selection) {
-    console.log(selection)
+    const computerSelection = randomSelection()
+    const yourWinner = Winner(selection, computerSelection)
+    const computerWinner = Winner(computerSelection, selection)
+    
+    addSelectionResult(computerSelection, computerWinner)
+    addSelectionResult(selection, yourWinner)
+}
+
+function addSelectionResult(selection, winner) {
+    const div = document.createElement('div')
+    div.innerText= selection.img
+    div.classList.add('result-selection')
+    if (winner) div.classList.add('winner')
+    finalColumn.after(div)
+
+}
+
+function Winner(selection, opponentSelection) {
+    return selection.beats === opponentSelection.name
+}
+
+function randomSelection() {
+  const randomIndex = Math.floor(Math.random() * SElECTIONS.length)
+  return SElECTIONS[randomIndex]
 }
