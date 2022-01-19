4a811b88bc07bf6c920a386f52f0e5515d2cc45c
Harriss1989 <harriss_1@hotmail.co.uk>
2022-01-19 20:19:05 +0000

    add close button functions

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,6 +3,7 @@ var modal = document.getElementById('player-input-modal');
 // Get open modal buttons
 var modalBtn = document.getElementById('new-game');
 var closeBtn = document.getElementsByClassName('closeBtn')[0];
+
 // listen for clicks
 modalBtn.addEventListener('click', openModal);
 closeBtn.addEventListener('click', closeModal);
@@ -11,9 +12,40 @@ window.addEventListener('click', outsideClick);
 function openModal(){
     modal.style.display = 'block';
 }
-function closeModal(...args: []) {
-    rulesModal.style.display = 'none';
+function closeModal(){
+    modal.style.display = 'none';
+}
+function outsideClick(e){
+    if(e.target == modal){
+      modal.style.display = 'none';
+    } 
+}
 
+// start game button
+var startBtn =document.getElementById('start-game');
+// Get game modal elements
+var gameModal = document.getElementById('selectionModal');
+//listen for start button click
+startBtn.addEventListener('click', startGameModal);
+// function to open new game
+function startGameModal(){
+    gameModal.style.display = 'block';
+}
+
+// get rules modal
+var rulesModal =document.getElementById('rules-container-modal');
+// rules button
+var rulesBtn = document.getElementById('rules');
+var closeBtn1 = document.getElementsByClassName('closeBtn1')[0];
+//listen for rules button click
+rulesBtn.addEventListener('click', openRulesModal);
+closeBtn1.addEventListener('click', closeModal1);
+
+function openRulesModal(){
+    rulesModal.style.display = 'block';
+}
+function closeModal1(){
+    rulesModal.style.display = 'none';
 }
 
 // settings button
